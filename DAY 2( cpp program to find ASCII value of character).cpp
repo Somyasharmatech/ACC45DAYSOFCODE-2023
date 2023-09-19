11c69b51a11673695ab60a1cf89c70f5c4e54bf9
somya sharma <114328620+Somyasharmatech@users.noreply.github.com>
2023-09-19 20:35:23 +0530

    Add files via upload

---
+++ DAY 2( cpp program to find ASCII value of character).cpp
@@ -0,0 +1,11 @@
+//DAY 2 PROGRAM TO FIND ASCII VALUE IN C++
+#include <iostream>
+using namespace std;
+
+int main() {
+ char c;
+ cout << "Enter a character: ";
+ cin >> c;
+ cout << "ASCII Value of " << c << " is " << int(c);
+ return 0;
+}
