423751215da28fddb0108e58e43acbecdd6ef51c
somya sharma <114328620+Somyasharmatech@users.noreply.github.com>
2023-10-14 21:32:30 +0530

    Add files via upload

---
+++ DAY 27 (Chef and Interactive Contests of codechef).py
@@ -0,0 +1,9 @@
+# DAY 27 Chef and Interactive Contests
+
+n,r=map(int,input().split())
+for i in range(n):
+    R=int(input())
+    if R<r:
+        print("Bad boi")
+    else:
+        print("Good boi")
