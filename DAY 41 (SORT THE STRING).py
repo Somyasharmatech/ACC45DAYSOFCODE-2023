37532c822d7fcdb5e08c43d98933ecc03d1c1785
somya sharma <114328620+Somyasharmatech@users.noreply.github.com>
2023-10-28 14:26:18 +0530

    Add files via upload

---
+++ DAY 41 (SORT THE STRING).py
@@ -0,0 +1,9 @@
+# DAY 41 SORT THE STRING
+for _ in range(int(input())):
+    n=int(input())
+    s=input()
+    count=0
+    for i in range(0,n-1):
+        if s[i:i+2]=="10":
+            count+=1
+    print(count)
