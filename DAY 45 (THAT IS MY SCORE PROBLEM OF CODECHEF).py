4849c8b3b6ce9e22c7f256d41ead758ac82fea80
somya sharma <114328620+Somyasharmatech@users.noreply.github.com>
2023-11-01 16:49:06 +0530

    Add files via upload

---
+++ DAY 45 (THAT IS MY SCORE PROBLEM OF CODECHEF).py
@@ -0,0 +1,9 @@
+# DAY 45 THAT IS MY SCORE  
+for i in range(int(input())):
+    n=int(input())
+    a=[0]*8
+    for j in range(n):
+        x,y=map(int,input().split())
+        if x<=8:
+            a[x-1]=max(a[x-1],y)
+    print(sum(a))
